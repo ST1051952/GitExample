7a7c7fc850fb4af63919d585827bee31fbdec87c
Student <Student@LabVM2047644.d03xqroc1ktubguio5royxpmte.jnbx.internal.cloudapp.net>
2026-04-01 10:35:44 +0200

    My first commit class

--- src/gitexample/GitExample.java
+++ src/gitexample/GitExample.java
@@ -15,6 +15,10 @@ public class GitExample {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        String name;
     }
     
 }
